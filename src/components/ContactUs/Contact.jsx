8cf4972a68a01d4856a80e96b2b0ccabe36c2edb
Oliv-19 <betza.bruguera@gmail.com>
2025-12-30 14:03:33 -0400

    update

--- src/components/ContactUs/Contact.jsx
+++ src/components/ContactUs/Contact.jsx
@@ -1,28 +1,42 @@
+import { useState } from 'react'
 import styles from './Contact.module.css'
 
 export function Contact(){
+    const [isSend, setIsSend] = useState(false)
+    const handleClick= (e) => {
+        e.preventDefault()
+        setIsSend(true)
+    }
      return (
     
         <main className={styles.main}>
             <h1>Contact Us</h1>
-            <form action="" className={styles.form}>
-                <label htmlFor="">
-                    Name
-                    <input type="text" name="name" id="" /> 
-                </label>
-                <label htmlFor="">
-                    Email
-                    <input type="email" name="name" id="" /> 
-                </label>
-                <div>
-                    <label htmlFor="" className={styles.textareaLabel}>
-                        Message
-                        <textarea rows='10' cols='45' /> 
-                    </label>
+            <div className={styles.box}>
+                {isSend? (
+                    <p>Message sent successfully</p>
 
-                </div>
-                <button onClick={(e)=> e.preventDefault()} className={styles.btn}>Send</button>
-            </form>
+                ):(
+                    <form action="" className={styles.form}>
+                        <label htmlFor="">
+                            Name
+                            <input type="text" name="name" id="" required/> 
+                        </label>
+                        <label htmlFor="">
+                            Email
+                            <input type="email" name="name" id="" required/> 
+                        </label>
+                        <div>
+                            <label htmlFor="" className={styles.textareaLabel} required>
+                                Message
+                                <textarea rows='10' cols='45' /> 
+                            </label>
+
+                        </div>
+                        <button onClick={handleClick} className={styles.btn}>Send</button>
+                    </form>
+                )}
+
+            </div>
         </main>
     )
 }
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -8,10 +8,9 @@ export default function HomePage(){
                 <div className={styles.title}>
                     <h1>SHOP</h1>
                     <Link to="/shop">Shop now</Link>
-
+                    <p>Photo by <Link to='https://unsplash.com/es/@anotherlovely' target="_blank">Alyssa Strohmann</Link></p>
                 </div>
                 <div className={styles.imgWrapper}>
-
                     <img src="https://images.unsplash.com/photo-1558769132-cb1aea458c5e?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className={styles.bg} />
                 </div>
 
